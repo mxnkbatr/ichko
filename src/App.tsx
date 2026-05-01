2f1c528948d39d0ba90beb7c14b4cc4a725a9fde
mxnkbatr <d.monkh2007@gmail.com>
2026-05-01 23:35:18 +0800

    fix: scroll to top on route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import { AppLayout } from './components/AppLayout'
 import { HomePage } from './pages/HomePage'
 import { PlaceDetailsPage } from './pages/PlaceDetailsPage'
@@ -10,22 +11,33 @@ import { AboutPage } from './pages/AboutPage'
 import { FiltersPage } from './pages/FiltersPage'
 import { PlaceReviewsPage } from './pages/PlaceReviewsPage'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+  return null
+}
+
 function App() {
   return (
-    <Routes>
-      <Route element={<AppLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="/filters" element={<FiltersPage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-        <Route path="/bookings" element={<BookingsPage />} />
-        <Route path="/me" element={<ProfilePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/place/:placeId" element={<PlaceDetailsPage />} />
-        <Route path="/place/:placeId/reviews" element={<PlaceReviewsPage />} />
-        <Route path="/place/:placeId/book" element={<BookingPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Route>
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/filters" element={<FiltersPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="/bookings" element={<BookingsPage />} />
+          <Route path="/me" element={<ProfilePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/place/:placeId" element={<PlaceDetailsPage />} />
+          <Route path="/place/:placeId/reviews" element={<PlaceReviewsPage />} />
+          <Route path="/place/:placeId/book" element={<BookingPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
